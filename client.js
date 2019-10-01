731521e3b547cab77308f3d375fb5a0dcd01afea
Luke Schleder <luke.schleder@gmail.com>
2019-10-01 17:51:32 -0500

    complete object-group-bonus-calculator

--- client.js
+++ client.js
@@ -33,6 +33,84 @@ const employees = [
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
+for (let i = 0; i < employees.length; i++) {
+  console.log(employees[i]);
+  console.log(bonusCalculator(employees[i]));
+  console.log(employeeBonus(employees[i]));
+}
+
+function bonusCalculator(onePerson) {
+  let percentage;
+
+  if (onePerson.reviewRating <= 2) {
+    percentage = 0;
+    
+
+  } else if (onePerson.reviewRating === 3) {
+    percentage = 4;
+    
+  } else if (onePerson.reviewRating === 4) {
+    percentage = 6;
+    
+  } else {
+    percentage = 10;
+    
+  }
+
+  if (onePerson.employeeNumber.length === 4) {
+    percentage += 5;
+    
+  }
+
+  if (Number(onePerson.annualSalary) > 65000) {
+    percentage -= 1;
+    
+  }
+
+  if (percentage > 13) {
+    percentage = 13;
+    
+  }
+  if (percentage < 0) {
+    percentage = 0
+    
+  }
+  return percentage;
+}
+
+function employeeBonus(onePerson) {
+  let newObject = {};
+  newObject.name= onePerson.name;
+  let percentage = bonusCalculator(onePerson);
+  let decimalPercentage= percentage * .01;
+  newObject.percentage = decimalPercentage;
+  let totalBonus = Math.round(decimalPercentage * onePerson.annualSalary);
+  newObject.totalBonus = totalBonus;
+  newObject.totalCompensation = totalBonus + Number(onePerson.annualSalary);
+
+  return newObject;
+  
+}
+console.log(employeeBonus(employees[1]));
+
+// bonusCalculator(employees[0]);
+// bonusCalculator(employees[1]);
+bonusCalculator(employees[2]);
+// bonusCalculator(employees[3]);
+// bonusCalculator(employees[4]);
+//   let ouput = {
+//     name: employees.name,
+//     bonusPercentage: percentage,
+//     totalCompensation: adjustedCompensation,
+//     totalBonus: totalBonus
+
+
+
+
+//   }
+//   return output
+// }
+
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
 // This problem is massive! Break the problem down. Use the debugger.
 // What is the fewest lines of code I can write and test to get just a little closer?
@@ -40,4 +118,4 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
